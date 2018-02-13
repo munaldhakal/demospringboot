504b3be91b67e3e08487de468aa0d1ddf5a091a1
Munal <dhakal.munal@gmail.com>
2018-02-13 17:02:12 +0545

    Cross Origin Added

--- src/main/java/com/example/test/demo/controller/UserController.java
+++ src/main/java/com/example/test/demo/controller/UserController.java
@@ -18,6 +18,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,12 +41,14 @@ public class UserController {
 	
 	@Autowired
 	UserService userService;
+	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Object> createUser(@RequestBody UserDto userDto){
 		userService.createUser(userDto);		
 		return new ResponseEntity<Object>(HttpStatus.CREATED);
 	}
-	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Object> getUser(@PathVariable Long id){
 		UserResponse user = userService.getUser(id);
